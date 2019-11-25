c8aabfd3e323dd0dc783c5e6eb6db4790733852c
Connor Dawson <connordawson@Connors-iMac.connect>
2019-11-25 16:42:55 +0000

    programs custom post type with relationship to events being displayed both ways

--- single-event.php
+++ single-event.php
@@ -17,6 +17,21 @@ get_header();
                 <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event');?>"><i class="fa fa-home" aria-hidden="true"></i>Events Home</a> <span class="metabox__main"><?php the_title();?> </span></p>
             </div>               
                 <?php the_content();?>
+                
+                <?php
+                    $relatedPrograms = get_field('related_programs');
+
+                    if ($relatedPrograms) {
+                        echo '<hr class="section-break">';
+                        echo '<h3 class="headline headline--medium">Related Programs</h3>';
+                        echo '<ul class="link-list min-list">';
+                        foreach ($relatedPrograms as $program) { ?>
+                            <li><a href="<?php echo get_the_permalink($program);?>"><?php echo get_the_title($program);?></a></li>
+                        <?php }
+                        echo '</ul>';
+                    }
+                ?>   
+                
             </div>
         </div>    
     <?php }
